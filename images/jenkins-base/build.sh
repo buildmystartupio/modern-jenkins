a959e05863c07e931ef89e6de2232979ba2f3927
James Knott <devops@buildmystartup.io>
2017-08-28 08:51:54 -0700

    updated build.sh

--- images/jenkins-base/build.sh
+++ images/jenkins-base/build.sh
@@ -1,13 +1,13 @@
-#!/bin/bash -el
-    # images/jenkins-base/build.sh
+#!/bin/bash 
+# images/jenkins-base/build.sh
 
-    # Define our image name
-    image_name=buildmystartup/jenkins-base:latest
+# Define our image name
+image_name=buildmystartup/jenkins-base:latest
 
     # Accept any args passed and add them to the command
-    docker image build ${@} -t $image_name $(dirname -- "$0")
+docker image build ${@} -t $image_name $(dirname -- "$0")
 
-    # If we add PUSH=true to the command, it will push to the hub
-    if [ "$PUSH" = true ] ; then
-      docker image push $image_name
-    fi
+# If we add PUSH=true to the command, it will push to the hub
+if [ "$PUSH" = true ] ; then
+   docker image push $image_name
+fi
